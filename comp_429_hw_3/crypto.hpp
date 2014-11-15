e89c77099ec7215b4d49238dfb503f0e3f6307be
Paul Kirth <pk1574@gmail.com>
2014-11-14 20:55:20 -0800

    Removed a bunch of commented out code, reformatted, fixed for loop in main.cpp

--- comp_429_hw_3/crypto.hpp
+++ comp_429_hw_3/crypto.hpp
@@ -20,119 +20,10 @@ class crypto
 	typedef std::vector<score> scores;
 
 private:
-	/*
-#pragma region List
-	 const static std::vector<std::string> lst = { "the",
-		"be",
-		"to",
-		"of",
-		"and",
-		"in",
-		"that",
-		"have",
-		"it",
-		"for",
-		"not",
-		"on",
-		"with",
-		"he",
-		"as",
-		"you",
-		"do",
-		"at",
-		"this",
-		"but",
-		"his",
-		"by",
-		"from",
-		"they",
-		"we",
-		"say",
-		"her",
-		"she",
-		"or",
-		"an",
-		"will",
-		"my",
-		"one",
-		"all",
-		"would",
-		"there",
-		"their",
-		"what",
-		"so",
-		"up",
-		"out",
-		"if",
-		"about",
-		"who",
-		"get",
-		"which",
-		"go",
-		"me",
-		"when",
-		"make",
-		"can",
-		"like",
-		"time",
-		"no",
-		"just",
-		"him",
-		"know",
-		"take",
-		"people",
-		"into",
-		"year",
-		"your",
-		"good",
-		"some",
-		"could",
-		"them",
-		"see",
-		"other",
-		"than",
-		"then",
-		"now",
-		"look",
-		"only",
-		"come",
-		"its",
-		"over",
-		"think",
-		"also",
-		"back",
-		"after",
-		"use",
-		"two",
-		"how",
-		"our",
-		"work",
-		"first",
-		"well",
-		"way",
-		"even",
-		"new",
-		"want",
-		"because",
-		"any",
-		"these",
-		"give",
-		"day",
-		"most",
-		"us",
-		"hello",
-		"world" };
-#pragma endregion List
-
-*/
-
 	
 	std::string top_alpha;// = "etaoinshr"; //{{ 'e', 't', 'a', 'o', 'i', 'n', 's', 'h', 'r' } };
 	std::unordered_map<std::string, size_t> hash, dict;
-	
-	
-	
-	
+
 	//static std::string str_inc(const std::string& input, int i);
 	static std::vector<int> prime_factors(int n);
 	//static std::vector<std::future<bool>> make_vec(std::vector<std::string> vec, std::string str);
@@ -142,10 +33,7 @@ private:
 	int get_scores(const std::string& str, size_t n);
     static bool comp(const score &x, const score &y);
 
-
-
-
-
+    
 public:
 	crypto();
 	//static std::vector<std::string> shift(const std::string& str);
--- comp_429_hw_3/main.cpp
+++ comp_429_hw_3/main.cpp
@@ -15,58 +15,9 @@ int main()
 	//system("pause");
 
 	//auto shifted = shift(msg);	
-	for (auto s : mapped)
-	{
-		/*
-		if (s == "helloworld")
-		{
-			std::cout << "Success!!!!" << std::endl;
-			system("pause");
-		}
-		else
-			std::cout << s << std::endl;
-		*/
-		solver.transpose(s);		
-	}
-	
-	//transpose(shifted[0]);
+	for (auto it = mapped.begin(); it != mapped.end(); ++it)
+		solver.transpose((*it));
+
 	system("pause");
 	return EXIT_SUCCESS;
 }
-
-/*for (auto rows = 1; rows < size; ++rows)
-	{
-		std::vector<std::string> words(rows);
-		int j;
-		for (int i = 0; i < size; ++i)
-		{
-			j = i % rows;
-			words[j].push_back(str[i]);
-		}
-		
-		for (auto s : words)
-			str_vec[rows-1] += s;
-	}
-	*/
-
-/*
-std::string s;
-s.reserve(str.size());
-int k = 0;
-int index = 0;
-bool even = false;
-//auto m = rows % size;
-for (auto j = 0; j < size; j++)
-{
-s.push_back(str[k]);
-if (!even && ((k + rows) >= size) && (size % (k + rows) == 0))
-even = true;
-k = (k + rows) % size;
-if (even && k == index)
-{
-k++;
-index++;
-}
-}
-str_vec[rows] = s;
-*/
